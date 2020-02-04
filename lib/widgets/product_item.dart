b68d96c911091731c90d43114c23243260706608
Kennedy <kennedymessias.vb@gmail.com>
2020-02-04 18:13:28 -0300

    ft/product item

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 
 class ProductItem extends StatelessWidget {
+  final String id;
+  final String title;
+  final String imageUrl;
+
+  ProductItem(this.id, this.imageUrl, this.title);
+
   @override
   Widget build(BuildContext context) {
     return GridTile(
-      child: Image.network(src),
+      child: Image.network(imageUrl),
     );
   }
 }
