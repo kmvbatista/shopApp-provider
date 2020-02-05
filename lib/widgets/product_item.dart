a91370c3aa8b95c7f42cdcb274d38a1e629f8a79
Kennedy <kennedymessias.vb@gmail.com>
2020-02-04 21:18:53 -0300

    styling grid items

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -9,16 +9,29 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridTile(
-      child: Image.network(
-        imageUrl,
-        fit: BoxFit.cover,
-      ),
-      footer: GridTileBar(
-        backgroundColor: Colors.black45,
-        title: Text(title, textAlign: TextAlign.center),
-        leading: IconButton(icon: Icon(Icons.favorite), onPressed: () {}),
-        trailing: IconButton(icon: Icon(Icons.shopping_cart), onPressed: () {}),
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        child: Image.network(
+          imageUrl,
+          fit: BoxFit.cover,
+        ),
+        footer: GridTileBar(
+          backgroundColor: Colors.black45,
+          title: Text(title, textAlign: TextAlign.center),
+          leading: IconButton(
+              icon: Icon(
+                Icons.favorite,
+                color: Theme.of(context).accentColor,
+              ),
+              onPressed: () {}),
+          trailing: IconButton(
+              icon: Icon(
+                Icons.shopping_cart,
+                color: Theme.of(context).accentColor,
+              ),
+              onPressed: () {}),
+        ),
       ),
     );
   }
