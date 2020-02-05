497f82b4b139aab3c16021eda17208fa78495d98
Kennedy <kennedymessias.vb@gmail.com>
2020-02-05 15:06:01 -0300

    working with multiProviders

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:shop/pages/product_detail.dart';
 import 'package:provider/provider.dart';
 import 'package:shop/pages/products_overview.dart';
+import 'package:shop/providers/cart.dart';
 import 'package:shop/providers/products.dart';
 
 void main() => runApp(MyApp());
@@ -10,8 +11,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      builder: (ctx) => Products(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider.value(value: Products()),
+        ChangeNotifierProvider.value(value: Cart())
+      ],
       child: MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
